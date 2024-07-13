782dc2774799af1cdf38b7315f53d490fda28197
kishore <kishore.ponnoju@aptiv.com>
2024-07-13 17:04:08 +0530

    DocString

---
+++ Demo_DocString.py
@@ -0,0 +1,12 @@
+""" Moduel demo code for Documenation in python."""
+
+def mutiply(firstarg,secondarg):
+    """
+    Get two interger and Peform Multiplication
+    :param firstarg:  This is frist Argument.
+    :param Secondarg: This is second Arugment.
+    :return: return the Muliplication result.
+    """
+    return int(firstarg*secondarg)
+
+print(mutiply(6,7))
