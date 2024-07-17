5b1131fa6dc5925ffd5f4a01ffb6b21a1cd3837d
kishore <kishore.ponnoju@aptiv.com>
2024-07-17 23:41:30 +0530

    Ping

--- Os_Ping.py
+++ Os_Ping.py
@@ -1,10 +1,10 @@
 import os
 
-command = 'ping -c 1 google.com'
+command = 'ping -n 1 google.com'
 
 result = os.system(command)
 
 if result != 0:
     print("command is not successful")
 else:
-    print(result)
+    print("Command is successful")
